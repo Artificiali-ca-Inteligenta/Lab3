80c4eb708463a5a2e713895fae4bf1f71d9a74ee
Stefan Cirstea <stefan.sc.sc@gmail.com>
2021-03-21 16:29:22 +0200

    desen

--- ex1.py
+++ ex1.py
@@ -1,4 +1,6 @@
 import csv
+import networkx as nx
+import matplotlib.pyplot as plt
 
 class Distanta:
         def __init__(self,location1, location2, distance):
@@ -11,7 +13,6 @@ class Distanta:
 
 
 def main():
-        print("Hello!")
         listaDistante = []
         with open('distante.csv', 'r') as file:
                 reader = csv.reader(file)
@@ -21,8 +22,20 @@ def main():
                         end = row[1]
                         dist = row[2]
                         listaDistante.append(Distanta(location1=begin, location2 = end, distance= dist))
+
+        G = nx.Graph()
         for dist in listaDistante:
-                print(str(dist))
+          G.add_edge(dist.location1,dist.location2)
+
+        H = nx.Graph()
+
+        print("Nodes: ")
+        print(G.nodes())
+        print("Edges: ")
+        print(G.edges())
+        
+        plt.axis("off")
+        plt.figure(figsize = (10,10))
+        nx.draw_networkx(G, node_size = 2500,font_size = 12, pos=nx.kamada_kawai_layout(G))
+        plt.savefig("grap.png")
 main()
-
-
